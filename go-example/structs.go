686e77243baaa816f9fa624ce78d3d3c83a7174c
oguzhalit <oguzhalit.sak@gmail.com>
2020-05-06 22:27:51 +0300

    yeni ornekler

---
+++ go-example/structs.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+type person struct {
+	name string
+	age int
+}
+
+func newPerson(name string) *person {
+	p := person{name: name}
+	p.age = 42
+	return &p
+}
+
+func main() {
+
+	fmt.Println(person{"Bob",20})
+	fmt.Println(person{name: "Alice", age:30})
+	fmt.Println(person{name: "Fred"})
+	fmt.Println(&person{name:"Ann",age: 30})
+	fmt.Println(newPerson("Jon"))
+
+	s := person{name: "oguz", age:20}
+	fmt.Println(s.name)
+
+	sp := &s
+	fmt.Println(sp.age)
+
+	sp.age = 51
+	fmt.Println(sp.age)
+	//yapılara tekrar dön bak
+}
